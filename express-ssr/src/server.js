2e008a460bfedf1bb1aee683d2fdae617bfe2dd0
dengwb <dengwenbin@wecash.net>
2020-03-21 21:01:23 +0800

    add express ssr

---
+++ express-ssr/src/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+var app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world!!</h1>')
+})
+
+app.listen(3000, function () {
+  console.log('server started!!')
+})
